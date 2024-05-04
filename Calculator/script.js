bc6d346bd9c7777d0fbd6d0515162ff1ce70987e
PreslavStamatov <preslav18122@abv.bg>
2024-05-04 17:21:23 +0300

    calc-update

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,29 @@
-document.querySelector('.calculator-screen').value = 0;
+const display = document.querySelector('.calculator-screen');
+
+const mathProblem = {
+  number1: '',
+  operator: '',
+  number2: ''
+}
+
+const clearScreen = function() {
+  mathProblem.number1 = '';
+  mathProblem.operator = '';
+  mathProblem.number2 = '';
+  display.value = '';
+  window.alert(mathProblem.number1)
+  
+}
+
+const updateCurrentDisplayValue = function(value) {
+  display.value += value;
+  mathProblem.number1 += value;
+  window.alert(mathProblem.number1);
+}; 
+
+const updateOperator = function(operator) {
+  
+  display.value += operator;
+  window.alert(operator)
+}
+
